fa911756626d51bda6072ea7efc3f61d8fb9f294
hungran <26101787+hungran@users.noreply.github.com>
2023-07-15 15:06:28 +0700

    Update variables.tf

--- terraform/ap-southeast-1/variables.tf
+++ terraform/ap-southeast-1/variables.tf
@@ -82,8 +82,8 @@ variable "eks" {
     vpc_name    = "demo-vpc"
     cluster_version = "1.25"
 
-    create_aws_auth_configmap = true
-    manage_aws_auth_configmap = false
+    create_aws_auth_configmap = false
+    manage_aws_auth_configmap = true
     irsa_enabled              = true
     irsa = {
       aws-ebs-csi-driver = {
