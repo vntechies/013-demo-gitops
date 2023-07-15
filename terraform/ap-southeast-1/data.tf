c5fabf908228b521117b827290d127e46d050849
hungran <26101787+hungran@users.noreply.github.com>
2023-07-15 14:39:42 +0700

    demo gitops

--- terraform/ap-southeast-1/data.tf
+++ terraform/ap-southeast-1/data.tf
@@ -1,9 +1,9 @@
-# data "aws_eks_cluster" "eks" {
-#   name       = module.eks.eks_cluster_name
-#   depends_on = [module.eks.eks_cluster_name]
-# }
+data "aws_eks_cluster" "eks" {
+  name       = module.eks.eks_cluster_name
+  depends_on = [module.eks.eks_cluster_name]
+}
 
-# data "aws_eks_cluster_auth" "eks" {
-#   name = module.eks.eks_cluster_name
-# }
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.eks_cluster_name
+}
 
